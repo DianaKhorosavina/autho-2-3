52d5f0de7e862b5e0b421acb962791c0447d5648
Diana Khorosavina <dkhorosavina@gmail.com>
2023-12-13 10:52:32 +0300

    test failed

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        var text = "Hello";
+        given()
+                .baseUri("https://postman-echo.com")
+                        .body("text")
+                        .when()
+                        .post("/post")
+                        .then()
+                        .statusCode(200)
+                        .body("data", equalTo(text));
+
+    }
+}
